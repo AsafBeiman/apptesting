fa8b8afcf1d29641547ebaea7e2a29978d95d640
AsafBeiman <66245633+AsafBeiman@users.noreply.github.com>
2024-12-31 15:29:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,7 +151,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from webdriver_manager.chrome import ChromeDriverManager
 import tempfile
 import subprocess
 from PIL import Image
